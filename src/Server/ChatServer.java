e1cf9b22e05a58ae0e812ecf1d389a44363d35e4
Vladimir <larsoni4@rambler.ru>
2019-03-22 16:41:39 +0300

    очередное не пойми что... vol.3

--- src/Server/ChatServer.java
+++ src/Server/ChatServer.java
@@ -29,39 +29,11 @@ public class ChatServer {
         }
     }
 
-    private static void listenToInputStream (Client client, Socket socket, ClientService clientService) throws IOException {
+    private static void listenToInputStream (Client client, Socket socket, ClientServiceImpl clientService) throws IOException {
         if (!socket.isClosed()) {
             String[] data = client.getIs().readUTF().split("&");
             System.out.println("listen " + data[0]);
             switch (data[0]) {
-                case "login":
-                    if (clientsDB.checkAuth(data[1], data[2])) {
-                        System.out.println("Client connected again: " + client + "::" + socket);
-                        clientStorage.addClient(client);
-                        client.getIs().writeUTF("logged");
-                        ClientServiceImpl.processMessage(data[1] + " enters chat.");
-                    } else {
-                        System.out.println("Attempt to login failed for " + data[1] + " " + data[2]);
-                        client.getOs().writeUTF("fail");
-                    }
-                    break;
-                case "logout":
-                    System.out.println("Client disconnected:" + client + "::" + socket);
-                    client.getOs().writeUTF("logout");
-                    clientStorage.removeClient(client);
-                    ClientServiceImpl.processMessage(data[1] + " leaves chat.");
-                    break;
-                case "logoutFull":
-                    System.out.println("Client is off.");
-                    try {
-                        System.out.println("Socket for " + client + " is off");
-                        ClientServiceImpl.processMessage(data[1] + " leaves chat.");
-                        socket.close();
-                    } catch (IOException e) {
-                        System.out.println("Unable to close socket");
-                        e.printStackTrace();
-                    }
-                    break;
                 case "message":
                     System.out.println("Received message");
                     ClientServiceImpl.processMessage(data[1]);
